35b1d1a97a811e367e6074ac3b71e6e46c999582
daniellejane-c <138036698+daniellejane-c@users.noreply.github.com>
2024-04-02 17:09:34 +0100

    get coordinates and place polygons on map

--- project1/libs/js/scripts.js
+++ project1/libs/js/scripts.js
@@ -52,11 +52,13 @@ var weatherBtn = L.easyButton("fa-info", function (btn, map) {
 
 // initialise and add controls once DOM is ready
 
+
+
 $(document).ready(function () {
   
   map = L.map("map", {
     layers: [streets]
-  }).setView([54.5, -4], 6);
+  }).setView([51.505, -0.09], 6);
   
   // setView is not required in your application as you will be
   // deploying map.fitBounds() on the country border polygon
@@ -67,6 +69,7 @@ $(document).ready(function () {
   weatherBtn.addTo(map);
   locationBtn.addTo(map);
 populateDropdown();
+showBorder();
 })
 
 function populateDropdown(){
@@ -88,6 +91,7 @@ function populateDropdown(){
 
   function getCountryInfo(countryName){
     //make a call to opencage to get country information.
+    //create php and then ajax call.
   }
 
   $(document).ready(function() {
@@ -101,3 +105,44 @@ function populateDropdown(){
     });
   });
   
+
+
+
+
+  function showBorder() {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+
+    // Use jQuery's $.ajax() to fetch multipolygon coordinates from PHP file
+    $.ajax({
+      dataType: "json",
+      url: "/clone/libs/php/countryBorders.geo.json",
+      success: function(data) {
+          //an array to hold coordinates for JavaScript
+          var coordinatesArray = [];
+
+          // Iterate through features
+          $.each(data.features, function(index, feature) {
+              // Extract coordinates
+              var coordinates = feature.geometry.coordinates;
+
+              // Push coordinates to the array
+              coordinatesArray.push(coordinates);
+
+              // Output country name, ISO_A3 code, and coordinates
+              console.log("Country: " + feature.properties.name);
+              console.log("ISO_A3: " + feature.properties.iso_a3);
+              console.log("Coordinates: " + JSON.stringify(coordinates) + "/n");
+          });
+
+          // Iterate through coordinates array and draw polygons
+          $.each(coordinatesArray, function(index, coordinates) {
+              var polygon = L.polygon(coordinates, {color: 'blue'}).addTo(map);
+          });
+      },
+      error: function(xhr, status, error) {
+          console.error("Error fetching GeoJSON data:", error);
+      }
+  });
+    };
+    
